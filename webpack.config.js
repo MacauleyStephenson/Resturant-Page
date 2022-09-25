9c1199cb8f39e7bdc20655d3cd248a778db36b77
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-09-25 13:35:08 +0100

    webpack working after a few required modules installed

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,26 @@ const path = require('path');
 
 module.exports = {
 	mode: "production",
-	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	entry: {
+		main: "./src/index.js",
+	},
+	devServer: {
+		contentBase: "./dist"
+	},
 	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ["style-loader", "css-loader"]
+		},
+		{
+			test: /\.(jpe?g|png)$/i,
+			type: "asset",
+		},
+		]
 	},
 };
