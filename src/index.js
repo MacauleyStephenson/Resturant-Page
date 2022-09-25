00bcb07ca294761463b7107651e44a48772bfcf6
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-09-25 12:57:30 +0100

    added base companents

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Test argument");
+import renderHome from "./pages/homePage";
+import renderMenu from "./pages/menuPage";
+import renderContact from './pages/contactPage.js';
+import { getHome, getMenu, getContact } from './components/navBar';
+
+renderHome();
+
+//Nav
+
+const content = document.querySelector("#content");
+
+const home = getHome()
+home.addEventListener("click", e => {
+	content.innerHTML = "";
+	renderHome();
+});
+
+const menu = getMenu();
+menu.addEventListener("click", e => {
+	content.innerHTML = "";
+	renderMenu();
+	console.log('something happened')
+});
+
+const contact = getContact();
+contact.addEventListener("click", e => {
+	content.innerHTML = "";
+	renderContact();
+})
